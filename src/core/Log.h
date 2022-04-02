0113d9dd89fd793da214ecab5d9b0e3072167994
DYNIO <dd7412506@gmail.com>
2022-04-02 22:43:30 +0200

    Quick fix

--- src/core/Log.h
+++ src/core/Log.h
@@ -2,8 +2,8 @@
 #define DENGINE_LOG_H
 #include "spdlog/spdlog.h"
 #define DENGINE_DEFUALT_LOGGER_NAME "DEngineLogger"
-#define DENGINE_ERROR(...) DEngine::LogManager::getLogger()->error(__VA_ARGS__)
-#define DENGINE_WARN(...) DEngine::LogManager::getLogger()->warn(__VA_ARGS__)
-#define DENGINE_INFO(...) DEngine::LogManager::getLogger()->info(__VA_ARGS__)
 #define DENGINE_TRACE(...) DEngine::LogManager::getLogger()->trace(__VA_ARGS__)
+#define DENGINE_INFO(...) DEngine::LogManager::getLogger()->info(__VA_ARGS__)
+#define DENGINE_WARN(...) DEngine::LogManager::getLogger()->warn(__VA_ARGS__)
+#define DENGINE_ERROR(...) DEngine::LogManager::getLogger()->error(__VA_ARGS__)
 #endif
