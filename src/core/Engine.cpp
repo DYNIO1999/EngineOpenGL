1d30139eae81bae6365da70a1c6c2f78a0fb12ff
DYNIO <dd7412506@gmail.com>
2022-04-22 15:02:34 +0200

    quick fix

--- src/core/Engine.cpp
+++ src/core/Engine.cpp
@@ -45,10 +45,13 @@ namespace DEngine{
         }
         //ECS
         //SCENE PUSHING
-        sceneManager.pushScene(new WaveSurfaceScene("Waves", window));
+
+        //sceneManager.pushScene(new WaveSurfaceScene("Waves", window));
         //sceneManager.pushScene(new TestScene("HELLOOO", window));
 
-        //sceneManager.pushScene(new GPUParticlesScene("RainParticles", window));
+        sceneManager.pushScene(new GPUParticlesScene("RainParticles", window));
+
+
         editorScenePtr =  new EditorScene(window);
         sceneManager.pushSceneOverlay(editorScenePtr);
         //SCENE PUSHING
