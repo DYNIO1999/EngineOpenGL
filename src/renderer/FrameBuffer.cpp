1a3e68a4f0f436988f768f3e58486b26fedf571b
DYNIO <dd7412506@gmail.com>
2022-05-05 10:06:34 +0200

    working on Framebuffers

--- src/renderer/FrameBuffer.cpp
+++ src/renderer/FrameBuffer.cpp
@@ -2,8 +2,11 @@
 
 namespace DEngine{
 
-    FrameBuffer::FrameBuffer() {
+    FrameBuffer::FrameBuffer(const FramebufferSpecification& specs) {
         glGenFramebuffers(1, &frameBufferID);
+
+
+        assert(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE, "Framebuffer is incomplete!");
     }
     FrameBuffer::~FrameBuffer() {
         glDeleteFramebuffers(1,&frameBufferID);
@@ -12,16 +15,18 @@ namespace DEngine{
     void FrameBuffer::bind() {
         glBindFramebuffer(GL_FRAMEBUFFER, frameBufferID);
 
-        glGenTextures(1, &textureColorbuffer);
-        glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
+        //glGenTextures(1, &textureColorbuffer);
+        //glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+        //glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+        //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        //glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
 
     }
     void FrameBuffer::unBind() {
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
     }
+    void FrameBuffer::bindTexture(uint slot) {
 
+    }
 }
--- src/renderer/FrameBuffer.h
+++ src/renderer/FrameBuffer.h
@@ -1,25 +1,36 @@
 #ifndef DENGINE_FRAMEBUFFER_H
 #define DENGINE_FRAMEBUFFER_H
+#include <assert.h>
 #include "core/Types.h"
 #include "Renderer.h"
+#include "core/LogManager.h"
+
 
 namespace  DEngine {
+
+
+    struct FramebufferSpecification
+    {
+        uint width = 0;
+        uint height = 0;
+        uint samples = 1;
+    };
 
     class FrameBuffer {
 
     public:
-        FrameBuffer();
+        FrameBuffer(const FramebufferSpecification& specs);
         ~FrameBuffer();
-
 
         void bind();
         void unBind();
-        void createColorAttachmentTexture(uint slot =0);
-        uint getColorAttachmentTexture() const { return  frameColorAttachmentTextureID;}
+        void bindTexture(uint slot =0);
+        //uint get() const { return  frameColorAttachmentTextureID;}
 
     private:
-        uint frameColorAttachmentTextureID;
+        uint frameBufferTextureID;
         uint frameBufferID;
+        FramebufferSpecification framebufferSpecification;
     };
 }
 #endif
